9331833369b2ec2bf5d83cb9b2cf21c95da8be4b
Evellyn <xe.uyemura@gmail.com>
2020-08-26 17:07:25 -0300

    colocando pedidos

---
+++ api/server/src/models/order.js
@@ -0,0 +1,12 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Order = sequelize.define('Order', {
+    is_active: DataTypes.BOOLEAN,
+    table: DataTypes.INTEGER,
+    client: DataTypes.STRING
+  }, {});
+  Order.associate = function(models) {
+    // associations can be defined here
+  };
+  return Order;
+};
